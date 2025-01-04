cebd631c2d380a7f00d85a1717b1bbee4c647fe3
Monika <monikathareja84@gmail.com>
2025-01-04 22:05:42 +1100

    GetMethod

--- EmployeeAdminPortal/Models/Entities/Employee.cs
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -6,6 +6,6 @@
         public required string Name { get; set; }
         public required string Email { get; set; }
         public  string? phone { get; set; }
-        public required decimal Salary { get; set; } 
+        public required double Salary { get; set; } 
     }
 }
